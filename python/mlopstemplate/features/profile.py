3d7f29b95adf50653a50c14d92ecc0e0aba73363
davitbzh <davit.bzhalava@gmail.com>
2023-10-20 14:22:36 +0200

    ondemand_features

--- python/mlopstemplate/features/profile.py
+++ python/mlopstemplate/features/profile.py
@@ -8,4 +8,4 @@ def select_features(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
     - DataFrame:
     """
-    return df[["cc_num", "cc_provider", "cc_type", "cc_expiration_date", "birthdate"]]
+    return df[["cc_num", "cc_provider", "cc_type", "cc_expiration_date", "birthdate", "country_of_residence"]]
